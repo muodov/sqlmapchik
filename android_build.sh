acb1422ac1ad5f86369a1ad474a43e597f0df018
muodov <muodov@gmail.com>
2013-12-14 01:23:29 +0400

    updated build script

--- android_build.sh
+++ android_build.sh
@@ -5,6 +5,6 @@ currentdir=`pwd`
 #rm $apkname
 pushd ~/android/python-for-android/dist/default/
 rm -rf ./bin/*
-./build.py --window --package com.muodov.sqlmapchik --name sqlmapchik --version 1.0 --dir "$currentdir" --icon "$currentdir/icon.png" --orientation sensor --permission INTERNET --permission WRITE_EXTERNAL_STORAGE debug installd
+./build.py --window --package com.muodov.sqlmapchik --name sqlmapchik --version 1.2.2 --dir "$currentdir" --compile-pyo --icon "$currentdir/icon.png" --presplash "$currentdir/icon.png" --orientation sensor --permission INTERNET --permission WRITE_EXTERNAL_STORAGE release
 popd
-cp "~/android/python-for-android/dist/default/bin/$apkname" ./
+#cp "~/android/python-for-android/dist/default/bin/$apkname" ./
