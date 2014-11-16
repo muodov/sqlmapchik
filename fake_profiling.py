763897a2231f92b3542282c339af9cb39e5317a6
hellman <hellman1908@gmail.com>
2014-11-16 23:27:25 +0300

    working buildozer spec

---
+++ fake_profiling.py
@@ -0,0 +1,6 @@
+"""
+Just a stub to replace original lib.core.profiling
+"""
+print 'fake profiling imported'
+def profile(profileOutputFile=None, dotOutputFile=None, imageOutputFile=None):
+    pass
